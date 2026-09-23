04c0ee33e564bcfaf67b2613861c28e0ae3f93de
Joao.V Grochovski D.O - 1.C <joao.grochovski@escola.pr.gov.br>
2026-09-23 08:42:54 -0300

    js

---
+++ script.js
@@ -0,0 +1,15 @@
+
+  const botoes = document.querySelectorAll('button');
+
+  botoes.forEach( function(botao){
+    botao.addEventListener('click', botaoClicado);
+
+
+  function botaoClicado(){
+    let clicks = botao.querySelector('span');
+    console.log("fui clicado");
+    clicks.textContent++; 
+  }
+  }
+  )
+  
